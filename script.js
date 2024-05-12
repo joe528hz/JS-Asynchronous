5f4080cb9d04e6ef22ea6fe0dd6c986b50216add
JoelMalazarte <joel.malazarte@proweaver.net>
2024-05-12 13:23:38 +0800

    Promises and the Fetch API/Consuming Promises

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ getCountryData('israel');
 
 ////////////////////////////////////////////////////////
 //Callback HELL
+/*
 const renderCountry = function (data, classname = '') {
   const html = `
   <article class="country ${classname}">
@@ -104,3 +105,54 @@ const getCountryAndNeigbor = function (country) {
 };
 
 getCountryAndNeigbor('usa');
+*/
+
+////////////////////////////////////////////////////////
+//Promises and the Fetch API
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+const renderCountry = function (data, classname = '') {
+  const html = `
+  <article class="country ${classname}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(data.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(data.currencies)[0].name
+      }</p>
+  </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('portugal');
