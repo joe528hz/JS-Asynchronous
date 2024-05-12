7e95a232b1a30e9fa94502ec65ef10c447af9052
JoelMalazarte <joel.malazarte@proweaver.net>
2024-05-12 13:55:56 +0800

    Chaining Promises

--- script.js
+++ script.js
@@ -150,9 +150,20 @@ const renderCountry = function (data, classname = '') {
 // };
 
 const getCountryData = function (country) {
+  // country1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+
+      // country2
+      const neighbour = data[0].borders?.[0];
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
 };
 
 getCountryData('portugal');
