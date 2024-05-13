e2125f3307018535be3225907b403942949057da
JoelMalazarte <joel.malazarte@proweaver.net>
2024-05-13 20:33:04 +0800

    Handling Rejected Promises

--- script.js
+++ script.js
@@ -5,6 +5,35 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, classname = '') {
+  const html = `
+  <article class="country ${classname}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(data.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(data.currencies)[0].name
+      }</p>
+  </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (message) {
+  countriesContainer.insertAdjacentText('beforeend', message);
+  // countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 // AJAX Call:XMLHttpRequest
 /*
@@ -113,30 +142,6 @@ getCountryAndNeigbor('usa');
 // request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 // request.send();
 
-const renderCountry = function (data, classname = '') {
-  const html = `
-  <article class="country ${classname}">
-  <img class="country__img" src="${data.flags.png}" />
-  <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${
-        Object.values(data.languages)[0]
-      }</p>
-      <p class="country__row"><span>💰</span>${
-        Object.values(data.currencies)[0].name
-      }</p>
-  </div>
-  </article>
-  `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.com/v3.1/name/${country}`)
 //     .then(function (response) {
@@ -163,7 +168,14 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data[0], 'neighbour'));
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      console.log(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try Again!`);
+    })
+    .finally((countriesContainer.style.opacity = 1));
 };
 
-getCountryData('portugal');
+btn.addEventListener('click', function () {
+  getCountryData('asdasd');
+});
