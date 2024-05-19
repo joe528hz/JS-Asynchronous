9c8399b9dab17f878c948d5fa3e3f98e38328259
JoelMalazarte <joel.malazarte@proweaver.net>
2024-05-19 22:42:28 +0800

    Challenge #2

--- script.js
+++ script.js
@@ -344,51 +344,122 @@ whereAmI(-33.933, 18.474);
 //   err => console.error(err)
 // );
 
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(
-      postition => resolve(postition),
-      err => reject(err)
-    );
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(
+//       postition => resolve(postition),
+//       err => reject(err)
+//     );
+//   });
+// };
+
+// // getPosition().then(response => console.log(response));
+// const whereAmI = function () {
+//   getPosition()
+//     .then(pos => {
+//       console.log(pos);
+//       const { latitude: lat, longitude: lng } = pos.coords;
+//       return fetch(
+//         `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+//       );
+//     })
+//     .then(response => {
+//       console.log(response);
+//       if (!response.ok)
+//         throw new Error(`Problem with geocoding${response.status}`);
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.countryName}`);
+
+//       return fetch(`https://restcountries.com/v3.1/name/${data.countryName}`)
+//         .then(response => {
+//           if (!response.ok)
+//             throw new Error(`Country not Found!, ${response.status}`);
+//           return response.json();
+//         })
+//         .then(data => {
+//           console.log(data);
+//           renderCountry(data[0]);
+//           countriesContainer.style.opacity = 1;
+//         });
+//     })
+//     .catch(err => {
+//       console.log(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try Again!`);
+//     });
+// };
+
+// btn.addEventListener('click', whereAmI);
+
+//////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
   });
 };
 
-// getPosition().then(response => console.log(response));
-const whereAmI = function () {
-  getPosition()
-    .then(pos => {
-      console.log(pos);
-      const { latitude: lat, longitude: lng } = pos.coords;
-      return fetch(
-        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
-      );
-    })
-    .then(response => {
-      console.log(response);
-      if (!response.ok)
-        throw new Error(`Problem with geocoding${response.status}`);
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      console.log(`You are in ${data.city}, ${data.countryName}`);
+const imgContainer = document.querySelector('.images');
 
-      return fetch(`https://restcountries.com/v3.1/name/${data.countryName}`)
-        .then(response => {
-          if (!response.ok)
-            throw new Error(`Country not Found!, ${response.status}`);
-          return response.json();
-        })
-        .then(data => {
-          console.log(data);
-          renderCountry(data[0]);
-          countriesContainer.style.opacity = 1;
-        });
-    })
-    .catch(err => {
-      console.log(`${err} 💥💥💥`);
-      renderError(`Something went wrong 💥💥 ${err.message}. Try Again!`);
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
     });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
 };
 
-btn.addEventListener('click', whereAmI);
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
